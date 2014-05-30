a9186bf6ff6f81b6fa95d7f16450ac021ddaf0eb
Jayanth Kottapalli <jayanth1991@hotmail.com>
2014-05-30 12:45:00 -0400

    Signup layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 TwitterMock::Application.routes.draw do
+	get "users/new"
+	
 	root 'index#home'
+	match '/signup',  to: 'users#new',		via: "get"
 	match '/about',   to: 'index#about',    via: "get"
 	match '/contact', to: 'index#contact',  via: "get"
 	# The priority is based upon order of creation: first created -> highest priority.
