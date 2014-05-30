8bb3462a3de324433f56b31aa68e022ff1d226d3
Jayanth Kottapalli <jayanth1991@hotmail.com>
2014-05-29 23:05:42 -0400

    Repo init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwitterMock::Application.config.secret_key_base = '14a3db81b13f330f9221dcddb86187e8b7271cdcc7115e5d8662ac340953d143e13ad37d054e827f37dcea125d2c17139ab8bb5f83adb764fcc42882a568f402'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TwitterMock::Application.config.session_store :cookie_store, key: '_Twitter_mock_session'
