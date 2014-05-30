16578723281c9e6531f940969a3607ff114bd2f6
Jayanth Kottapalli <jayanth1991@hotmail.com>
2014-05-30 14:16:55 -0400

    Syntax bug fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,21 +5,22 @@ class UsersController < ApplicationController
 			params.require(:user).permit(:name, :email, :password, :password_confirmation)
 		end
 
-	def show
-		@user = User.find(params[:id])
-	end
-
-	def new
-		@user = User.new
-	end
-
-	def create
-		@user = User.new(user_params)
-		if @user.save
-			flash[:success] = "Welcome to my Twitter Mockup!"
-			redirect_to @user
-		else
-			render 'new'
+	public
+		def show
+			@user = User.find(params[:id])
 		end
-	end
+
+		def new
+			@user = User.new
+		end
+
+		def create
+			@user = User.new(user_params)
+			if @user.save
+				flash[:success] = "Welcome to my Twitter Mockup!"
+				redirect_to @user
+			else
+				render 'new'
+			end
+		end
 end
